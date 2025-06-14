222c0e7cf22fc8fea0a6abc3b71a6923d8ab2ac3
Sébastien Bouez <39953434+sebbouez@users.noreply.github.com>
2025-06-14 12:07:42 +0200

    Fix main.py

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ import re
 
 def define_env(env):
 
-    def on_page_markdown(markdown, **kwargs):
+    def on_page_markdown(markdown, page, config, **kwargs):
         # Rechercher toutes les balises <!-- md:version v="..." -->
         def replace_note(match):
             # Extraire le contenu de l'attribut text
             text = match.group(1)
             return f'<div><a class="note-apply-version" href="/finko-docs/updates/{text}/">Applies to {text}</a></div>'
-
+    
         # Remplace toutes les balises personnalisées
         markdown = re.sub(r'<!--\s*md:version\s+v="([^"]+)"\s*-->', replace_note, markdown)
-
+    
         return markdown
 
     env.on_page_markdown(on_page_markdown)
